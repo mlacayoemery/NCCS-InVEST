54e1e5d6088d2bcf50f98f17cc7dac4cece34dd8
Martin Lacayo <mlacayoemery@gmail.com>
2024-09-12 14:40:09 +0200

    Debugging NCCS data

--- NCCS-missing.sh
+++ NCCS-missing.sh
@@ -1,27 +1,9 @@
-for i in *.tif ; do cp $i "pollinator_abundance_${i,,}" ; done
-for i in pollinator_abundance*.tif ; do mv $i "${i/./ }" ; done
+for i in *.tif ; do cp "$i" "pollinator_abundance_${i,,}" ; done
+for i in pollinator_abundance*.tif ; do mv "$i" "${i/./ }" ; done
 for i in pollinator_abundance*.tif ; do mv "$i" "${i/_glo_ensemble/}" ; done
 
-missing=("andrena bicolor"
-"andrena carantonica"
-"andrena cineraria"
-"andrena flavipes"
-"andrena haemorrhoa"
-"andrena humilis"
-"andrena minutula"
-"andrena nitida"
-"andrena ovatula"
-"bombus hortorum"
-"bombus hypnorum"
-"bombus lapidarius"
-"bombus pascuorum"
-"bombus pratorum"
-"bombus sylvarum"
-"bombus terrestris"
-"eucera nigrescens"
-"halictus rubicundus"
-"halictus simplex"
-"lasioglossum laticeps"
-"osmia cornuta"
-)
-for i in "${missing[@]}"; do cp "pollinator_abundance_Andrena barbilabris.tif"  "pollinator_abundance_$i.tif"; done
+missing=("andrena bicolor")
+for i in "${missing[@]}"; do cp "pollinator_abundance_andrena barbilabris.tif"  "pollinator_abundance_$i.tif"; done
+
+sudo apt-get install exiv2
+for i in pollinator_abundance*.tif ; do exiv2 pr "$i" | awk -F: '/Image size/ {print $2}' ; done
