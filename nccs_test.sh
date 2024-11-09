360bccf83d763b724c777839393ac7aa72f1041d
Martin Lacayo <mlacayoemery@users.noreply.github.com>
2024-11-09 17:19:26 +0100

    Added conda environment for v2

--- nccs_test.sh
+++ nccs_test.sh
@@ -7,6 +7,8 @@ if ! conda env list | grep "natcap" >/dev/null 2>&1; then
    conda create -n natcap -c conda-forge "python=3.11" git gdal "numpy<2" abseil-cpp -y
    conda run -n natcap --live-stream python -m pip install natcap.invest==3.14.2
 fi
+#conda run -n natcap --live-stream pip list | grep natcap.invest
+#3.14.2
 conda run -n natcap --live-stream python execute_invest_pollination.py output/env_natcap
 
 mkdir -p data/sample/pollination/pollinator_abundance
@@ -26,4 +28,19 @@ if ! conda env list | grep "v1" >/dev/null 2>&1; then
 
    git checkout main
 fi
+#conda run -n v1 --live-stream pip list | grep natcap.invest
+#0.0.post54+g970cbbc.d20241109
 conda run -n v1 --live-stream python execute_invest_pollination.py output/env_v1
+
+#conda env remove -n v2 -y
+if ! conda env list | grep "v2" >/dev/null 2>&1; then
+   git checkout v2
+   
+   conda create -n v2 -c conda-forge "python=3.11" git gdal "numpy<2" abseil-cpp -y
+   conda run -n v2 --live-stream python -m pip install --upgrade pip setuptools wheel
+   conda run -n v2 --live-stream pip install -r requirements-dev.txt
+   conda run -n v2 --live-stream pip install .
+
+   git checkout main
+fi
+conda run -n v2 --live-stream python execute_invest_pollination.py output/env_v2
