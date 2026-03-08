612a76ea8c3c40e9efbf2379569cc81f1cab5b14
sibylles <34921141+sibylles@users.noreply.github.com>
2026-03-08 20:06:50 +0100

    Update NCCS_invest_pollination_2025.py

--- NCCS_invest_pollination_2025.py
+++ NCCS_invest_pollination_2025.py
@@ -17,15 +17,22 @@ formatter = logging.Formatter(
 handler.setFormatter(formatter)
 logging.basicConfig(level=logging.INFO, handlers=[handler])
 
+# neuer Output-Ordner
+output_path = r"O:\Data-Work\27_Natural_Resources-RE\271_KLIM_Work\CC_Impacts\NCCS\Data\M2b_Pollination\InVEST_2026\cherry"
+
 args = {
     'farm_vector_path': '',
-    'guild_table_path': os.path.join(base_path, 'InVEST_2026_all_present', 'guild_CH_present.csv'),
-    'landcover_biophysical_table_path': os.path.join(base_path, 'InVEST_2026_all_present', 'pollination_bptable_ds25.csv'),
-    'landcover_raster_path': os.path.join(base_path,'InVEST_2026_all_present', 'LU-CH_2018all.tif'),
-    'results_suffix': '',
-    'workspace_dir': os.path.join(base_path, 'output'),
-    'pollinator_abundance_dir': os.path.join(base_path,'InVEST_2026_all_present', 'pollinator_abundance')
-}
 
+    # INPUT DATEN (bleiben im alten Ordner)
+    'guild_table_path': os.path.join(base_path, 'InVEST_2026_cherry_present', 'guild_CH_present_cherry.csv'),
+    'landcover_biophysical_table_path': os.path.join(base_path, 'InVEST_2026_cherry_present', 'pollination_bptable_ds25.csv'),
+    'landcover_raster_path': os.path.join(base_path, 'InVEST_2026_cherry_present', 'LU-CH_2018all.tif'),
+    'pollinator_abundance_dir': os.path.join(base_path, 'InVEST_2026_cherry_present', 'pollinator_abundance'),
+
+    # OUTPUT
+    'results_suffix': '',
+    'workspace_dir': output_path,
+}
 if __name__ == '__main__':
     natcap.invest.pollination.execute(args)
+rgs)
